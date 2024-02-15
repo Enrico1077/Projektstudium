898572aee15c6dfbe17e7e7c85f3dbfe60d5022b
lukas-brameyer <149958821+lukas-brameyer@users.noreply.github.com>
2024-02-15 15:03:53 +0100

    Update anlageneffektivitaet.component.ts

--- src/app/anlageneffektivitaet/anlageneffektivitaet.component.ts
+++ src/app/anlageneffektivitaet/anlageneffektivitaet.component.ts
@@ -47,7 +47,7 @@ export class AnlageneffektivitaetComponent implements OnInit {
   spindleTimeRelative: number = 0;
   public meinWert: number = 80; //Test-Variable (Prozent vom Kreisdiagramm)
   data_request = {
-    MachinID: 1,
+    MaschineID: 1,
   };
 
 
