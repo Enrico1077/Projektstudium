02b9357e42d62c7b85d71c7268f088fdbdf2b548
SatansEnte <150006506+SatansEnte@users.noreply.github.com>
2024-02-19 00:39:59 +0100

    Darkmodus Update 2

--- src/app/einstellungen/einstellungen.component.ts
+++ src/app/einstellungen/einstellungen.component.ts
@@ -5,9 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './einstellungen.component.html',
   styleUrls: ['./einstellungen.component.scss']
 })
+
 export class EinstellungenComponent {
   toggleDarkMode(): void {
     const body = document.body;
     body.classList.toggle('dark_mode');
+  
+    const headerComponent = document.querySelector('app-header');
+    if (headerComponent) {
+      const logo = headerComponent.querySelector('.hedelius-logo') as HTMLImageElement;
+      if (body.classList.contains('dark_mode')) {
+        const darkImage = logo.dataset['dark'];
+        if (darkImage) {
+          logo.src = darkImage;
+        }
+      } else {
+        logo.src = 'assets/img/logo.png';
+      }
+    }
   }
 }
