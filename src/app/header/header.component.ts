f47e40a58e46489cd9c57720d032f9516b816a5d
SatansEnte <150006506+SatansEnte@users.noreply.github.com>
2024-02-22 16:00:08 +0100

    Header Updates von vorher

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
   ngAfterViewInit(): void {
     const burger = document.querySelector('.burger') as HTMLElement;
     const nav = document.querySelector('.nav-links') as HTMLElement;
@@ -16,34 +15,19 @@ export class HeaderComponent {
       nav.classList.toggle('nav-active');
       dropdown.classList.toggle('dropdown-active');
       burger.classList.toggle('toggle');
+
+      // Move the .nav-links to the .dropdown-content
+      if (dropdown.children.length === 0) {
+        while (nav.firstChild) {
+          dropdown.appendChild(nav.firstChild);
+        }
+      } else {
+        while (dropdown.firstChild) {
+          nav.appendChild(dropdown.firstChild);
+        }
+      }
     });
 
-    // Define a function 
-    function toggleNav() {
-      // Select your element
-      const nav = document.querySelector('.nav') as HTMLElement;
-
-      // Set the display style of the nav element based on its current value
-      nav.style.display = nav.style.display === 'none' ? 'flex' : 'none';
-
-      // Select all the elements inside the navigation links
-      const links = document.querySelectorAll(".nav ul li a");
-
-      // Remove the active class from each element
-      links.forEach(function(link) {
-        link.classList.remove("active");
-      });
-
-      // Select the element that matches the current URL
-      const currentLink = document.querySelector(".nav ul li a[href='" + window.location.pathname + "']");
-
-      // If a matching element is found, add the active class to it
-      if (currentLink) {
-        currentLink.classList.add("active");
-      }
-    }
-
-    // Call the toggleNav function
-    toggleNav();
+    // ...
   }
 }
